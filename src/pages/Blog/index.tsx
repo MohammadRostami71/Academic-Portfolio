ba799910adeb9fdc1e24c04c9851707c468857b3
mrostami0 <mohammad.rostami13@gmail.com>
2023-03-07 23:49:20 +0330

    💄 blog data config

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
+import blogData from './../../config/blogData.json';
 import useStyles from "./useStyles";
+import {Box} from "@mui/material";
+
+interface blog {
+    title: string,
+    id: number,
+    shortDescription: string,
+    description: string,
+    date: string,
+}
 
 const Blog: React.FC = () => {
     const classes= useStyles;
+    const blogInfo =blogData as blog[];
 
     return(
-        <></>
+        <Box>
+            {/*{blogInfo.map()}*/}
+        </Box>
     )
 };
 
