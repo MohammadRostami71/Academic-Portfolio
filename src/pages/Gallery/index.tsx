1cc2f01934665fa5ad6f148ee367a8ca7bea56eb
mrostami0 <mohammad.rostami13@gmail.com>
2023-03-29 11:36:19 +0430

    💄: dyanmically import images

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,27 +1,32 @@
-import React, { useState} from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import {Box} from "@mui/material";
-import image1 from '../../assets/images/gallery/slide-1.jpg'
-import image2 from '../../assets/images/gallery/slide-2.jpg'
-import image3 from '../../assets/images/gallery/slide-3.jpg'
-import image4 from '../../assets/images/gallery/slide-4.jpg'
 import useStyles from "./useStyles";
+
+let listOfImages : string[] =[];
 
 const Gallery: React.FC = () => {
     const classes= useStyles();
     const [nav1, setNav1] = useState();
     const [nav2, setNav2] = useState();
 
-    const images: any = [image1,image2,image1,image3,image4];
+    const importAll = (r:any) => {
+        return r.keys().map(r);
+    };
+
+    useEffect(() => {
+        // @ts-ignore
+        listOfImages = importAll(require.context('../../assets/images/gallery', false, /\.(png|jpe?g|svg)$/));
+    },[]);
 
     return(
         <Box className={classes.container}>
             <Box>
                 <Slider asNavFor={nav2} ref={(slider1:any) => setNav1(slider1)}>
-                    {images.map((image:string) => <img src={image} alt=' '/>)}
+                    {listOfImages.map((image:string,index:number) => <img key={index} src={image} alt=' '/>)}
                 </Slider>
             </Box>
 
@@ -34,7 +39,7 @@ const Gallery: React.FC = () => {
                     focusOnSelect={true}
                     className={classes.thumbnailSlider}
             >
-                {images.map((image:string) => <img src={image} alt=' '/>)}
+                {listOfImages.map((image:string) => <img src={image} alt=' '/>)}
             </Slider>
         </Box>
     )
